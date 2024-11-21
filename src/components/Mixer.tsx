1bc88bcc3a9c597898a3ed8fbfd4a8e784df6082
stevebarakat <stevebarakat@gmail.com>
2024-11-21 10:02:04 -0500

    co

--- src/components/Mixer.tsx
+++ src/components/Mixer.tsx
@@ -1,16 +1,29 @@
 import Track from "./Track";
 import { MixerContext } from "../machines/mixerMachine";
+import { ActorRefFrom } from "xstate";
+import { trackMachine } from "@/machines/trackMachine";
 
 function Mixer() {
   const { trackActorRefs, message } = MixerContext.useSelector(
     (state) => state.context,
   );
 
-  console.log("message", message);
+  const tracks = trackActorRefs?.map(
+    (trackActor: ActorRefFrom<typeof trackMachine>, i: number) => {
+      return (
+        <div key={i}>
+          <Track trackActor={trackActor} />
+        </div>
+      );
+    },
+  );
 
-  return trackActorRefs?.map((trackActor, i: number) => (
-    <Track key={i} trackActor={trackActor} />
-  ));
+  return (
+    <div className="flex flex-col gap-4">
+      <div>{message}</div>
+      <div className="flex gap-4">{tracks}</div>
+    </div>
+  );
 }
 
 export default Mixer;
